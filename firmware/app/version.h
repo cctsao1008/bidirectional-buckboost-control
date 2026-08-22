778371d70b2e21cda7abe25db40eddf2cb180045
Cheng <chiacheng.tsao@gmail.com>
2026-08-22 22:55:04 +0800

    firmware: add initial firmware version definitions

---
+++ firmware/app/version.h
@@ -0,0 +1,17 @@
+/**
+ * @file version.h
+ * @brief Firmware version identifiers used by the host protocol.
+ */
+
+#ifndef BUCKBOOST_VERSION_H
+#define BUCKBOOST_VERSION_H
+
+#define FIRMWARE_VERSION_MAJOR 0U
+#define FIRMWARE_VERSION_MINOR 1U
+#define FIRMWARE_VERSION_PATCH 0U
+
+#ifndef FIRMWARE_BUILD_ID
+#define FIRMWARE_BUILD_ID 0U
+#endif
+
+#endif /* BUCKBOOST_VERSION_H */
